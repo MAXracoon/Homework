3b7dfa87cb47dec21a6586810fa6b7528e3a1379
MAXracoon <143587600+MAXracoon@users.noreply.github.com>
2023-09-19 16:53:33 +0900

    9/19

--- Assets/Scripts/Controllers/TopDonwCharacterControllers.cs
+++ Assets/Scripts/Controllers/TopDonwCharacterControllers.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,6 +14,9 @@ public class TopDonwCharacterControllers : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(x, y) * Time.deltaTime;
     }
 }
